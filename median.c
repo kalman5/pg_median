0ceadbe1c8aec470539cf1d7442b76e3296c8539
kalman <mendola@gmail.com>
2019-10-01 23:56:50 +0200

    It should work with a fixed amount of data, I have a corruption on the current state, need to investigate why.

--- median.c
+++ median.c
@@ -16,7 +16,7 @@ typedef struct DatumChainNode
 } DatumChainNode;
 
 typedef struct MedianState {
-  int num_elemens;
+  int num_elements;
   struct DatumChainNode* root;
 } MedianState;
 
@@ -31,39 +31,50 @@ Datum
 median_transfn(PG_FUNCTION_ARGS)
 {
     MemoryContext agg_context;
+    
+    MedianState *median_state;
+    DatumChainNode* new_node;
 
     if (!AggCheckCallContext(fcinfo, &agg_context))
         elog(ERROR, "median_transfn called in non-aggregate context");
+    
+    median_state = (MedianState *) PG_GETARG_POINTER(0);
 
-    bool first_argument = PG_ARGISNULL(0);
+    if (median_state == NULL) { // First time the function is invoked
+      // Prepare state
+      median_state = (MedianState *) palloc(sizeof(MedianState));
+      median_state->num_elements = 0;
+      median_state->root = NULL;
+    } 
+      
+    if (!PG_ARGISNULL(1)) {
+      median_state->num_elements += 1;
+      
+      // Append new node in front of chain
+      new_node = (DatumChainNode *) palloc(sizeof(DatumChainNode));
+      new_node->datum = PG_GETARG_DATUM(1); // here median_state is corrupted!
+      new_node->next = median_state->root;
+      
+      median_state->root = new_node;
+    } 
 
-    if (first_argument) {
-      MedianState *median_state = (MedianState *) palloc(sizeof(MedianState));
-      if (!PG_ARGISNULL(1)) {
-        median_state->num_elemens = 1;
-        median_state->root = (DatumChainNode *) palloc(sizeof(DatumChainNode));
-        median_state->root->datum = PG_GETARG_DATUM(1);
-        median_state->root->next = NULL;
-      } else {
-        median_state->num_elemens = 0;
-        median_state->root = NULL;
-      }
-      PG_RETURN_POINTER(median_state);
-    } else {
-      MedianState *median_state = (MedianState *) PG_GETARG_POINTER(0);
-      if (!PG_ARGISNULL(1)) {
-          median_state->num_elemens += 1;
-          if (median_state->root == NULL) {
-              median_state->root = (DatumChainNode *) palloc(sizeof(DatumChainNode));
-              median_state->root->datum = PG_GETARG_DATUM(1);
-              median_state->root->next = NULL;
-          } else {
-          }
-      }
-      PG_RETURN_POINTER(median_state);
-    }
+    PG_RETURN_POINTER(median_state);
+}
 
-    PG_RETURN_NULL();
+int compare_datum(const void* left, const void* right);
+
+int compare_datum(const void* left, const void* right) {
+  Datum l = *(const Datum*)left;
+  Datum r = *(const Datum*)right;
+  
+  if (l < r) {
+    return -1;
+  } 
+  else if (l > r) {
+    return 1;
+  }
+  
+  return 0;
 }
 
 PG_FUNCTION_INFO_V1(median_finalfn);
@@ -79,10 +90,48 @@ Datum
 median_finalfn(PG_FUNCTION_ARGS)
 {
     MemoryContext agg_context;
+    MedianState *median_state;
+    Datum* datum_array;
+    Datum* array;
+    DatumChainNode* node;
+    DatumChainNode* node_to_free;
+    int num_elements;
+    int center_element;
 
     if (!AggCheckCallContext(fcinfo, &agg_context))
         elog(ERROR, "median_finalfn called in non-aggregate context");
+    
+    elog(INFO, "final call");
+    
+    median_state = (MedianState *) PG_GETARG_POINTER(0);
+    
+    if (median_state == NULL) {
+      PG_RETURN_NULL();
+    }
+    
+    num_elements = median_state->num_elements;
+    center_element = num_elements/2;
+    
+    array = datum_array = palloc(sizeof(Datum) * num_elements);
+    
+    node = median_state->root;
+    
+    while (node != NULL) {
+      *datum_array = node->datum;
+      ++datum_array;
+      node_to_free = node;
+      node = node->next;
+      pfree(node_to_free);
+    }
+    
+    pfree(median_state);
+    
+    qsort(array, num_elements, sizeof(Datum), compare_datum);
 
-    PG_RETURN_NULL();
+    if (num_elements % 2 == 0) {
+      PG_RETURN_DATUM((array[center_element]+array[center_element-1])/2);
+    } 
+    
+    PG_RETURN_DATUM(array[center_element]);
 }
 
